7f823d052d8eed6dc5a1049c795c7a1983fbd916
Kiyan <kiyankastudio@gmail.com>
2026-05-09 10:56:33 +0530

    update practice.py

--- practice.py
+++ practice.py
@@ -6,6 +6,7 @@ numbers = [0, 1, 2, 3, 4, 5, 6, 7,34, 11, 13, 14]
 m= len(numbers) // 2
 print(numbers[m-2 :m+2])
 
+
 fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 print(fruits[len(fruits)-3])
 
@@ -42,3 +43,26 @@ print(tasks)
 letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 for i in range(1, 8, 2):
     print(letters[i])
+
+nums = [1, 2, 3, 4, 5]
+nums[2:3]= [30, 40 , 50]
+print(nums)
+
+a = [1, 2, 3, 4, 5]
+b = a
+a.clear()
+print(a)
+print(b)
+
+scores = [85, 92, 78, 96, 88, 73, 91]
+scores.sort(reverse=True)
+print(scores)
+
+original = [3, 1, 4, 1, 5, 9, 2, 6]
+ascending =sorted(original)
+print(ascending)
+print(original)
+
+words = ["banana", "fig", "elderberry", "date", "kiwi"]
+words.sort(key=len)
+print(words)
